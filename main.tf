c8bf79de7109593a29c2628d0d7229fd27909665
samccet <50578841+peringangichandrakala@users.noreply.github.com>
2021-07-28 13:00:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "my_vm" {
+  project      = var.project
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+  network_interface {
+    network    = var.network
+    subnetwork = var.subnetwork
+  }
+}
