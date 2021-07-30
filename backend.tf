df8800e6bcb5abb4c7d73412dd2b02fefa98b027
chandrakala <50578841+peringangichandrakala@users.noreply.github.com>
2021-07-30 11:35:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
     backend "remote" {
         hostname = "app.terraform.io"
-        organization = "chandrakala"
+        organization = "<organisation name>"
 
         workspaces {
-            name = "sentinel-test"
+            name = "<workspace name>"
         }
     }
 }
